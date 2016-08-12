c8065d12f92710be01736a80650c914d962cddab
iuunhao <html.5@foxmail.com>
2016-08-12 18:16:28 +0800

    update

--- install.sh
+++ install.sh
@@ -1,37 +1,65 @@
 #!/usr/bin/env bash
 
 
-# ohmyzsh
-# echo "Remove .zshrc..." 
-# rm ~/.zshrc
-echo "Link .zshrc..." 
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-echo "=============================================="
 
 
-# vim
-# echo "Remove .vimrc..." 
-# rm ~/.zshrc
-echo "Link .vimrc..." 
+
+echo "安装Homebrew..."
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+echo "下载文件..."
+git clone git@github.com:iuunhao/dotfiles.git ~/
+
+echo "安装Oh My ZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "安装Node..."
+brew install node
+
+echo "安装node插件..."
+sudo npm i -g jshint csscomb js-beautify jsonlint http-server gulp webpack
+
+brew install ack
+brew install htop
+brew install tree
+brew install tmux
+brew install macvim
+brew install ctags
+brew install tig
+brew install node
+
+echo "移删vimrc文件..."
+rm ~/.zshrc
+echo "链接vimrc文件..."
 ln -s ~/dotfiles/vim/.vimrc ~/.vimrc
-echo "=============================================="
-echo "Done!" 
 
+echo "下载vundle..."
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
-# tmux 
-# echo "Remove .tumx.conf..." 
-# rm ~/.tumx.conf
-echo "Link .tumx.conf..." 
-ln -s ~/dotfiles/.tumx.conf ~/.tumx.conf
-echo "=============================================="
-echo "Done!" 
+echo "复制代码片段..."
+cp ~/dotfiles/vim/UltiSnips ~/.vim/UltiSnips
 
-
-# bash_profile 
-# echo "Remove .tumx.conf..." 
-# rm ~/.tumx.conf
-echo "Link .bash_profile..." 
+echo "链接bash_profile..."
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
-echo "=============================================="
-echo "Done!" 
+
+echo "移除tumx.conf..."
+rm ~/.tumx.conf
+echo "链接tumx.conf..."
+ln -s ~/dotfiles/.tumx.conf ~/.tumx.conf
+
+echo "移除zshrc..."
+rm ~/.zshrc
+echo "链接zshrc..."
+ln -s ~/dotfiles/.vimrc ~/.zshrc
+
+isContinue='y'
+echo "请您打开vim 进入进入命令模式执行":BundleInstall"等待安装完成！按y继续进行!"
+read -p 'continue?[y/n]' isContinue
+echo ${isContinue}
+
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --all
+
+echo "安装完成"
 
