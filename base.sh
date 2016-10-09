8fb35d54af437f4d352773a2bfa48b86164ccbcf
iuunhao <html.5@foxmail.com>
2016-10-09 17:51:21 +0800

    DEV UPDATE

--- base.sh
+++ base.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
-echo "The installation Homebrew..."
+
+mkdir -p ~/.config/
+
+echo "安装Homebrew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "The download file..."
-git clone git@github.com:iuunhao/dotfiles.git ~/
-
-echo "The installation Oh My ZSH..."
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "The installation Node..."
-brew install node
-
-echo "The installation node The plug-in..."
-sudo npm i -g jshint csscomb js-beautify jsonlint http-server gulp webpack
-
+echo "安装对应组件"
 brew install ack
 brew install htop
 brew install tree
@@ -23,22 +14,22 @@ brew install ctags
 brew install tig
 brew install node
 
-gem install markdown2html
+echo "安装npm模块"
+sudo npm i -g jshint csscomb js-beautify jsonlint http-server gulp webpack
 
-echo "Remove delete vimrc file..."
+echo "下载配置文件"
+git clone git@github.com:iuunhao/dotfiles.git ~/
+
+
+echo "移除并创建vimrc文件"
 rm ~/.zshrc
-echo "Link vimrc file..."
 ln -s ~/dotfiles/vim/vimrc.vim ~/.vimrc
 
-echo "Download vundle..."
+echo "下载vim-plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 echo "Copy the code fragment..."
-cp ~/dotfiles/UltiSnips ~/.vim/UltiSnips
+ln -s  ~/dotfiles/UltiSnips ~/.vim/UltiSnips
 
-echo "Remove ZSHRC..."
-rm ~/.zshrc
-echo "Link ZSHRC..."
-ln -s ~/dotfiles/zshrc.zsh ~/.zshrc
 
