199b735521cf65d376659cf01384e211c40176bb
iuunhao <html.5@foxmail.com>
2017-04-19 17:59:48 +0800

    DEV UPDATE

---
+++ scripts/link.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+rm ~/.zshrc
+rm ~/.vimrc
+rm ~/.config/nvim
+
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/vim/init.vim ~/.vimrc
+ln -s ~/dotfiles/vim/init.vim ~/init.vim
+ln -s ~/.vim/ ~/.config/nvim
+ln -s  ~/dotfiles/UltiSnips/ ~/.vim/UltiSnips
+
+#插件安装
+vim +PlugInstall +qall
