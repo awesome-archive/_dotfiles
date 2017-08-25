f46f0cbb385ea6434e4a06eb617de06e11cba24a
iuunhao <html.5@foxmail.com>
2017-08-25 13:37:04 +0800

    installvim sh

---
+++ scripts/installvim.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+~
+
+git clone git@github.com:iuunhao/dotfiles.git ~/dotfiles/
+
+echo '安装vim-plug'
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo '链接vimrc'
+ln -s ~/dotfiles/vim/init.vim ~/.vimrc
